b7f5a85147dce46499c9b6502d8666d989e3e846
Heldigrad <maria.agape@student.tuiasi.ro>
2023-05-02 09:33:51 +0300

    Implement actor

--- Lab10/ppl10/src/main/kotlin/Main.kt
+++ Lab10/ppl10/src/main/kotlin/Main.kt
@@ -1,12 +1,28 @@
 package com.pp.laborator
 import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.actor
 import kotlinx.coroutines.sync.Mutex
 import kotlin.system.*
-suspend fun CoroutineScope.massiveRun(action: suspend () -> Unit){val n = 100
+
+sealed class ContorMsg
+object IncContor : ContorMsg()
+class GetContor(val response: CompletableDeferred<Int>) : ContorMsg()
+
+fun CoroutineScope.counterActor() = actor<ContorMsg> {
+    var contor = 0
+    for (msg in channel) {
+        when (msg) {
+            is IncContor -> contor++
+            is GetContor -> msg.response.complete(contor)
+        }
+    }
+}
+
+suspend fun CoroutineScope.massiveRun(action: suspend () -> Unit){
+    val n = 100
     val k = 1000
     val time = measureTimeMillis {
-        val jobs = List(n)
-        {
+        val jobs = List(n) {
             launch { repeat(k) { action() } }
         }
         jobs.forEach { it.join() }
@@ -15,7 +31,9 @@ suspend fun CoroutineScope.massiveRun(action: suspend () -> Unit){val n = 100
 }
 val mtContext = newFixedThreadPoolContext(2, "mtPool")
 var counter = 0
+
 fun main() = runBlocking<Unit> {
+    /* subpunctul 1
     val m = Mutex()
     CoroutineScope(mtContext).massiveRun {
         m.lock()
@@ -23,4 +41,16 @@ fun main() = runBlocking<Unit> {
         m.unlock()
     }
     println("Numarator = $counter")
+    */
+
+    val contor = counterActor()
+    GlobalScope.massiveRun()
+    {
+        contor.send(IncContor)
+        println(contor.onSend)
+    }
+    val raspuns = CompletableDeferred<Int>()
+    contor.send(GetContor(raspuns))
+    println("Contor = ${raspuns.await()}")
+    contor.close()
 }
