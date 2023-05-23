0b811a5acbf38dfa1c7387b4a3c70e346f9b04e8
Heldigrad <maria.agape@student.tuiasi.ro>
2023-05-23 08:55:12 +0300

    Adaugare ex 2

--- Lab13/ex1/main.py
+++ Lab13/ex1/main.py
@@ -9,3 +9,14 @@ class int(int):
 
 print("is 5 prime? " + str(int(5).is_prime()))
 print("is 6 prime? " + str(int(6).is_prime()))
+
+#ex2
+class str(str):
+    def toPascalCase(self):
+        l = list(self.split(" "))
+        return "".join(map(lambda x: x.capitalize(), l))
+
+if __name__ == '__main__':
+# se pot crea extensii DOAR pe clase custom
+    text = str("pushi este pisica mea mai crazy asa de fel")
+    print(text.toPascalCase())
